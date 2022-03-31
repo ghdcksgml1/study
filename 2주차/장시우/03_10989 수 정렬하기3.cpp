686a56da9e7ee1d6b37bdf3edf42db72e0c6dd8b
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-04-01 02:09:24 +0900

    03_10989 수 정렬하기3
    03_10989 수 정렬하기3

---
+++ 2주차/장시우/03_10989 수 정렬하기3.cpp
@@ -0,0 +1,29 @@
+﻿// 언어 : C++ , (성공/실패) : 1/2 , 메모리 : 2020 KB , 시간 : 1776ms
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+
+	int n; cin >> n;
+	int count[10001] = { 0 };
+
+	for (int i = 0; i < n; i++) {
+		int t;
+		cin >> t;
+		count[t] += 1;
+	}
+
+	for (int i = 1; i < 10001; i++) {
+		for (int j = 0; j < count[i]; j++)
+			cout << i << "\n";
+	}
+
+	return 0;
+
+}
+
