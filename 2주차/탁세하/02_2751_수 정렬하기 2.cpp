80bab650e1f58e6fa120f7115e1329242a9785ca
saesang <86196342+saesang@users.noreply.github.com>
2022-04-01 10:33:27 +0900

    Update 02_2751_수 정렬하기 2.cpp
    코드 주석처리 수정

--- 2주차/탁세하/02_2751_수 정렬하기 2.cpp
+++ 2주차/탁세하/02_2751_수 정렬하기 2.cpp
@@ -1,23 +1,23 @@
-//#include <iostream>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//int arr[1000000];
-//
-//int main() {
-//	ios::sync_with_stdio(0);
-//	cin.tie(0); cout.tie(0);
-//
-//	int N; cin >> N;
-//
-//	for (int i = 0; i < N; i++) {
-//		cin >> arr[i];
-//	}
-//
-//	sort(arr, arr + N);
-//
-//	for (int i = 0; i < N; i++) {
-//		cout << arr[i] << '\n';
-//	}
-//}
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1000000];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int N; cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + N);
+
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << '\n';
+	}
+}
