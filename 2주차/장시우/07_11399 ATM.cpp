31bc3df821abab7cd0d531944f249e4eb2a84c96
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-04-05 16:56:35 +0900

    Update 07_11399 ATM.cpp

--- 2주차/장시우/07_11399 ATM.cpp
+++ 2주차/장시우/07_11399 ATM.cpp
@@ -1,8 +1,13 @@
-﻿// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2020 KB , 시간 : 0ms
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2020 KB , 시간 : 0ms
+
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
+
+const int MAX = 1000;
+
+int n;
 
 int main()
 {
@@ -10,24 +15,32 @@ int main()
 	cin.tie(0); cout.tie(0);
 
 
-	int n; cin >> n;
-	int* time = new int[n];
+	int time[MAX];
+
+	cin >> n;
+	
 	for (int i = 0; i < n; i++) {
-		int t;
-		cin >> t;
-		time[i]=t;
+		cin >> time[i];
 	}
 
 	sort(time, time + n, greater<int>());
+	// 내림차순 정렬 greater<자료형>()
 
-	int t2=0,sum=0;
+	int sum=0;
 
 	for (int i = 0; i < n; i++)
 	{
-		sum += (i + 1) * time[i]; //
+		sum += (i + 1) * time[i];
 	}
+	/*예를 들어, sort 후에{1, 2, 3, 4, 5}일 때, 
+	1은 5번 더해지고 2는 4번, 3은 3번, 4는 2번, 5는 1번 더해진다.
+	그걸 이용해서  sum += (i + 1) * time[i]; 로 해주면 된다
+	= 그리디 알고리즘
+	*/
+
 	cout << sum<<"\n";
 	return 0;
 
 }
+
 
