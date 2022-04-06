d4ffd49d81c32770c40e54326557c07ea49b3261
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-04-06 20:01:22 +0900

    06_1764 듣보잡
    06_1764 듣보잡

---
+++ 2주차/장시우/06_1764 듣보잡.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<string, int> um;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector <string> v;
+    for (int i = 0; i < n; i++) {
+        string tmp = "";
+        cin >> tmp;
+        um[tmp] = 1;
+    }
+    for (int i = 0; i < m; i++) {
+        string tmp = "";
+        cin >> tmp;
+        if (um[tmp] == 1) {
+            v.push_back(tmp);
+        }
+    }
+    cout << v.size()<<"\n";
+    sort(v.begin(), v.end());
+    for (auto x : v) {
+        cout << x << "\n";
+    }
+    return 0;
+}
