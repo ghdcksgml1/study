b21afb1fda5736f9b8a737f26bfb1e7110489146
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-04-07 13:29:29 +0900

    Update 07_11399 ATM.cpp

--- 2주차/장시우/07_11399 ATM.cpp
+++ 2주차/장시우/07_11399 ATM.cpp
@@ -2,45 +2,38 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-const int MAX = 1000;
+vector<int> v;
 
-int n;
+int main() {
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
+    int n; cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        v.push_back(t);
+    }
 
-	int time[MAX];
+    sort(v.begin(), v.end());
 
-	cin >> n;
-	
-	for (int i = 0; i < n; i++) {
-		cin >> time[i];
-	}
+    int sum=0,acc=0;
 
-	sort(time, time + n, greater<int>());
-	// 내림차순 정렬 greater<자료형>()
+    for (int i = 0; i < n; i++) {
+        acc += v[i];
+        sum += acc;
+    }
+        
 
-	int sum=0;
+    cout << sum << "\n";
 
-	for (int i = 0; i < n; i++)
-	{
-		sum += (i + 1) * time[i];
-	}
-	/*예를 들어, sort 후에{1, 2, 3, 4, 5}일 때, 
-	1은 5번 더해지고 2는 4번, 3은 3번, 4는 2번, 5는 1번 더해진다.
-	그걸 이용해서  sum += (i + 1) * time[i]; 로 해주면 된다
-	= 그리디 알고리즘
-	*/
+    return 0;
 
-	cout << sum<<"\n";
-	return 0;
 
 }
-
-
