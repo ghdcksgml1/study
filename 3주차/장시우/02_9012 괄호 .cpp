8f73a602e0c68ae27aada41a05adec0c2bbe77f5
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-04-12 23:54:53 +0900

    02_9012 괄호
    02_9012 괄호

---
+++ 3주차/장시우/02_9012 괄호 .cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    string s, ans = "";
+    stack<char> st;
+    cin >> n;
+    while (n--)
+    {
+        cin >> s;
+        stack<char> st;
+        for (const char& c : s)
+        {
+            if (c == '(')
+            {
+                st.push(c);
+            }
+            else
+            {
+                if (!st.empty())
+                {
+                    st.pop();
+                }
+                else
+                {
+                    st.push('(');
+                    break;
+                }
+            }
+        }
+        if (!st.empty())
+        {
+            ans += "NO\n";
+        }
+        else
+        {
+            ans += "YES\n";
+        }
+    }
+    cout << ans;
+}
