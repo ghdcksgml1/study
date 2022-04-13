6c1fb80dbea3508e0c8d6906715205b16ee062d7
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-04-13 12:27:09 +0900

    05_18258 큐2
    05_18258 큐2

---
+++ 3주차/장시우/05_18258 큐2.cpp
@@ -0,0 +1,59 @@
+﻿// 언어 : C++, (성공 / 실패) : 1 / 1, 메모리 : 8416kb, 시간 : 380ms
+
+#include<iostream>
+#include<stdio.h>
+#include<queue>
+using namespace std;
+
+int N, x;
+string s;
+queue<int> q;
+
+int main()
+{
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> s;
+
+		if (s == "push")
+		{
+			cin >> x;
+			q.push(x);
+		}
+		else if (s == "pop")
+		{
+			if (!q.empty())
+			{
+				cout << q.front() << '\n';
+				q.pop();
+			}
+			else cout << -1 << '\n';
+
+		}
+		else if (s == "size")
+		{
+			cout << q.size() << '\n';
+		}
+		else if (s == "empty")
+		{
+			cout << q.empty() << '\n';
+		}
+		else if (s == "front")
+		{
+			if (!q.empty()) cout << q.front() << '\n';
+			else cout << -1 << '\n';
+		}
+		else // s == "back"
+		{
+			if (!q.empty()) cout << q.back() << '\n';
+			else cout << -1 << '\n';
+		}
+	}
+
+}
