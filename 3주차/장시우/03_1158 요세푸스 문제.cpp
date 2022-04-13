2cead6456327c5e123ed4f4747037f3e693196ef
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-04-13 14:38:23 +0900

    03_1158 요세푸스 문제
    03_1158 요세푸스 문제

---
+++ 3주차/장시우/03_1158 요세푸스 문제.cpp
@@ -0,0 +1,28 @@
+﻿// 언어 : C++, (성공 / 실패) : 1 / 1, 메모리 : 2152kb, 시간 : 60ms
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, k;
+	queue<int> circle;
+	cin >> n >> k;
+	cout << "<";
+	for (int i = 0; i < n; i++)
+		circle.push(i+1);
+	while (circle.size() - 1) {
+		for (int i = 0; i < k - 1; i++) {
+			circle.push(circle.front());
+			circle.pop();
+		}
+		cout << circle.front() << ", ";
+		circle.pop();
+	}
+	cout << circle.front() << ">";
+}
