51fd45edb53aa6a5ab83b59ceb12e4ebb1380de0
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-05-03 16:29:33 +0900

    01_2960_에라토스테네스의 체
    01_2960_에라토스테네스의 체

---
+++ 4주차/장시우/01_2960_에라토스테네스의 체.cpp
@@ -0,0 +1,31 @@
+﻿// 언어 : C++, (성공 / 실패) : 1 / 0, 메모리 : 2020kb, 시간 : 0ms
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int n, k, cnt = 0;
+	cin >> n >> k;
+
+	vector<int> v(n + 1);
+
+	for (int i = 0; i < n; i++)
+		v[i] = i;
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 2; j <= n; j += i) {
+			if (v[j] != 0) {
+				v[j] = 0;
+				cnt++;
+				if (cnt == k)
+					cout << j << '\n';
+			}
+		}
+	}
+
+	return 0;
+}
