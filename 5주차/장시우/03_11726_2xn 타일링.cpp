59e0cd4ce20bc8b10bfa02d0fc92c9804f61fa8d
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-05-09 14:20:37 +0900

    03_11726_2xn 타일링
    03_11726_2xn 타일링

---
+++ 5주차/장시우/03_11726_2xn 타일링.cpp
@@ -0,0 +1,26 @@
+﻿// 언어 : C++, (성공 / 실패) : 1 / 0, 메모리 : 2020kb, 시간 : 0ms
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> dp;
+	dp.push_back(1);
+	dp.push_back(2);
+	for (int i = 2; i < n; i++) {
+		dp.push_back((dp[i - 1] + dp[i - 2]) % 10007);
+	}
+	cout << dp[n - 1];
+
+	return 0;
+}
