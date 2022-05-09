99b6e0f26c35a3914d0ad2488a5516c5b71fcb03
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-05-09 13:44:24 +0900

    02_1010_다리 놓기
    02_1010_다리 놓기

---
+++ 5주차/장시우/02_1010_다리 놓기.cpp
@@ -0,0 +1,33 @@
+﻿// 언어 : C++, (성공 / 실패) : 1 / 0, 메모리 : 2020kb, 시간 : 0ms
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int t, n, m;
+	long long num = 1;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		num = 1;
+		cin >> n >> m;
+
+		if (n > (m / 2)) {
+			n = m - n;
+		}
+		for (int j = m; j > m - n; j--) {
+			num *= j;
+		}
+		for (int j = n; j >0; j--) {
+			num /= j;
+		}
+
+		cout << num << '\n';
+	}
+
+	return 0;
+}
