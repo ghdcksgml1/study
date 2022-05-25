b17a68a9ffbb5f3b5189d8fb28d1255a8dfd1fe0
siwooJang <88125431+siwooJang@users.noreply.github.com>
2022-05-25 19:25:36 +0900

    05_두 수의 합
    05_두 수의 합

---
+++ 6주차/장시우/05_두 수의 합.cpp
@@ -0,0 +1,42 @@
+﻿// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2920 KB , 시간 : 12ms
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false); 
+	cin.tie(0); cout.tie(0);
+
+	int num, a, target;
+	cin >> num;
+	vector<int> v;
+	for (int i = 0; i < num; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+
+	cin >> target;
+	sort(v.begin(), v.end());
+	int l = 0, r = num - 1, cnt = 0;
+
+	while (1) {
+		if (l >= r)
+			break;
+		int sum = v[l] + v[r];
+		if (sum == target) {
+			cnt++;
+			l++;
+			r--;
+		}
+		else if (sum < target)
+			l++;
+		else
+			r--;
+	}
+	cout << cnt;
+	return 0;
+}
